c7f469139154ba348b1ca483771bf60ef0b10aeb
blasecato <bl.calderon@udla.edu.co>
2022-06-09 23:41:35 -0500

    fix(product list): list product by id and all entries

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 
 const ProductPage = () => {
   const { query } = useRouter()
+  const [productID, setProductID] = useState<TProduct>()
 
+  useEffect(() => {
+    window
+      .fetch(`/api/avo/${query.id}`)
+      .then((response) => response.json())
+      .then(({data, length}) => {
+        setProductID(data)
+      })
+  }, [])
+  console.log("avocado.name",productID && productID.name)
   return (
     <section>
       <h1>Página producto: {query.id}</h1>
+      {productID?.name}
     </section>
   )
 }
